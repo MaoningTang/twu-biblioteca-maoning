01405bd414367c4cb8c9a1500e6967612a179f22
Maoning Tang <maoning.tang@Maonings-MacBook-Pro.local>
2020-02-11 01:05:57 +0800

    [#1.2_Create_Library_class]

---
+++ src/com/twu/biblioteca/Library.java
@@ -0,0 +1,32 @@
+package com.twu.biblioteca;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Library {
+    private static Library library_instance = null;
+    private ArrayList<Book> books;
+
+    private Library() {
+        books = new ArrayList<Book>(Arrays.asList(new Book("Water Margin"),
+                new Book("The Journey to the West"),
+                new Book("A Dream of Red Mansions"),
+                new Book("Romance of the Three KingdomsThe Three Kingdoms Era")));
+    }
+
+    public ArrayList<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(ArrayList<Book> books) {
+        this.books = books;
+    }
+
+    public static Library getInstance() {
+        if (library_instance == null)
+            library_instance = new Library();
+
+        return library_instance;
+    }
+
+}
