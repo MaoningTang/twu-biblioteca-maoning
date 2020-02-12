af9f3e620c306864ee43397643894f19b9e285de
Maoning Tang <maoning.tang@Maonings-MacBook-Pro.local>
2020-02-13 02:28:47 +0800

    [#1.3 Printer implementation]

--- src/com/twu/biblioteca/MassagePrinter.java
+++ src/com/twu/biblioteca/MassagePrinter.java
@@ -1,9 +1,6 @@
 package com.twu.biblioteca;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.HashMap;
+import java.util.*;
 import java.util.stream.Stream;
 
 public class MassagePrinter {
@@ -32,32 +29,50 @@ public class MassagePrinter {
         }
     }
 
-    public static String getBoxFormatMessageForArrayList(HashMap<String,String[]>){
+    public static String getBoxFormatMessageForArrayList(String[][] bookMatrix){
         StringBuilder builder = new StringBuilder();
-        int boxWidth = getBoxWidth(messages);
-        constructBoxBoard(builder, boxWidth,"*");
-        for(String message:messages){
-            builder.append(System.lineSeparator());
-            builder.append("*");
-            builder.append(message);
-            for (int i = message.length()+1; i < boxWidth-1; i++) {
-                builder.append(" ");
-            }
-            builder.append("*");
-            appendSectionIndicator(builder, boxWidth);
+        int[] columnsWidth = new int[bookMatrix.length];
+        for(int i = 0; i<columnsWidth.length; i++){
+            String[] column = getColumn(i,bookMatrix);
+            columnsWidth[i] = getBoxWidth(column);
         }
-        deleteLastLine(builder);
+        int lineWidth = Arrays.stream(columnsWidth).sum() + 2*columnsWidth.length + 2;
+        constructBoxBoard(builder, lineWidth,"=");
         builder.append(System.lineSeparator());
-        constructBoxBoard(builder, boxWidth,"*");
-        builder.append(System.lineSeparator());
+        for(String[] messages:bookMatrix) {
+            builder.append("|");
+            for (int i = 0; i < messages.length; i++) {
+                String message = getLineFillingWithMarksToLength(messages[i], columnsWidth[i], " ");
+                message = getStringWithMarkOnEachSide(message, "|");
+                builder.append(message);
+            }
+            builder.append("|");
+            builder.append(System.lineSeparator());
+            constructBoxBoard(builder, lineWidth,"=");
+            builder.append(System.lineSeparator());
+        }
+        return builder.toString();
+    }
+
+    private static String getStringWithMarkOnEachSide(String message,String mark){
+        StringBuilder builder=new StringBuilder();
+        builder.append(mark);
+        builder.append(message);
+        builder.append(mark);
+        return builder.toString();
+    }
+
+    private static String getLineFillingWithMarksToLength(String message,int length,String mark){
+        StringBuilder builder=new StringBuilder();
+        builder.append(mark);
+        builder.append(message);
+        String toAppend = String.join("", Collections.nCopies(length - message.length() - 1, mark));
+        builder.append(toAppend);
         return builder.toString();
     }
 
     private static int getBoxWidth(String[] messages) {
         int boxWidth = Stream.of(messages).mapToInt(message -> message.length()).max().getAsInt() + 2;
-        if (boxWidth<=56){
-            boxWidth = 56;
-        }
         return boxWidth;
     }
 
@@ -76,5 +91,9 @@ public class MassagePrinter {
         if (last >= 0) {
             builder.delete(last, builder.length());
         }
+    }
+
+    private static String[] getColumn(int address, String[][] matrix) {
+        return Arrays.stream(matrix).map(x -> x[address]).toArray(String[]::new);
     }
 }
